57d80dead82ae47637dbb53f1fc0de042efa3ab7
Steffen Hemer <s.hemer@phytec.de>
2026-06-11 12:00:23 +0200

    sdl: examples: dm_firmware: makefile.mk: Fix LNKCMD paths
    Signed-off-by: Steffen Hemer <s.hemer@phytec.de>

--- sdl/examples/dm_firmware/common/makefile.mk
+++ sdl/examples/dm_firmware/common/makefile.mk
@@ -59,11 +59,11 @@ ifeq ($(BUILD_OS_TYPE), freertos)
     INCDIR += $(IPC_COMMON_PATH)/$(SOC)/$(BUILD_OS_TYPE)/
     SRCS_COMMON += c7x_mmu.c
   endif
-  EXTERNAL_LNKCMD_FILE_LOCAL = $(PDK_INSTALL_PATH)/../../sdl/examples/dm_firmware/common/$(SOC)/$(BUILD_OS_TYPE)/linker_$(ISA)_$(BUILD_CORE)_$(BUILD_OS_TYPE).lds
-  APPEND_LNKCMD_FILE += $(PDK_INSTALL_PATH)/../../sdl/examples/dm_firmware/common/$(SOC)/$(BUILD_OS_TYPE)/memory_map_ddr.cmd
+  EXTERNAL_LNKCMD_FILE_LOCAL = $(sdl_PATH)/examples/dm_firmware/common/$(SOC)/$(BUILD_OS_TYPE)/linker_$(ISA)_$(BUILD_CORE)_$(BUILD_OS_TYPE).lds
+  APPEND_LNKCMD_FILE += $(sdl_PATH)/examples/dm_firmware/common/$(SOC)/$(BUILD_OS_TYPE)/memory_map_ddr.cmd
   ifeq ($(ECHO_TEST_BTCM), 1)
     ifeq ($(ISA), r5f)
-      EXTERNAL_LNKCMD_FILE_LOCAL = $(PDK_INSTALL_PATH)/../../sdl/examples/dm_firmware/common/$(SOC)/$(BUILD_OS_TYPE)/linker_$(ISA)_$(BUILD_CORE)_btcm_$(BUILD_OS_TYPE).lds
+      EXTERNAL_LNKCMD_FILE_LOCAL = $(sdl_PATH)/examples/dm_firmware/common/$(SOC)/$(BUILD_OS_TYPE)/linker_$(ISA)_$(BUILD_CORE)_btcm_$(BUILD_OS_TYPE).lds
     endif
   endif
 endif
